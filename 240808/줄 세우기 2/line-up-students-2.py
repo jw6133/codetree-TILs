0b3b239f5687f5e9e2f89eb5f532c21a044f3b2a
BackG <jw6133@naver.com>
2024-08-08 16:08:42 +0900

    Title: 줄 세우기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240808/줄 세우기 2/line-up-students-2.py
@@ -0,0 +1,34 @@
+def lining(copy,a):
+    for j in range(a):
+        if(j+1==a):
+            break
+        elif copy[j][0]>copy[j+1][0]:
+            temp=copy[j]
+            copy[j]=copy[j+1]
+            copy[j+1]=temp
+            lining(copy,a)
+        elif copy[j][0]==copy[j+1][0]:
+            if(copy[j][1]<copy[j+1][1]):
+                tempe=copy[j]
+                copy[j]=copy[j+1]
+                copy[j+1]=tempe
+                lining(copy,a)
+
+a=int(input())
+base=[]
+
+cnt=1
+
+for i in range(a):
+    ar = list(map(int, input().split()))
+    base.append(ar)
+
+copy=base
+for i in copy:
+    i.append(cnt)
+    cnt=cnt+1
+
+lining(copy,a)
+
+for h in copy:
+    print(*h)
