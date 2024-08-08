0e877738a98064f236d6f8206b4ec20ba43ce818
BackG <jw6133@naver.com>
2024-08-08 15:23:57 +0900

    Title: 중앙값 계산 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240808/중앙값 계산 2/get-median-2.py
@@ -0,0 +1,13 @@
+a=int(input())
+
+ar = list(map(int, input().split()))
+
+result = []
+
+for i in range(a):
+    if((i+1)%2==1):
+        slic=ar[:i+1]
+        n=slic[len(slic)//2]
+        result.append(n)
+
+print(*result)
