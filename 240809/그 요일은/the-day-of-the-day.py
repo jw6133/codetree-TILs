f99f9bea984845917e0e956393a4fdeb0baba311
BackG <jw6133@naver.com>
2024-08-09 18:00:44 +0900

    Title: 그 요일은, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240809/그 요일은/the-day-of-the-day.py
+++ 240809/그 요일은/the-day-of-the-day.py
@@ -3,7 +3,7 @@ day = str(input())
 
 thirty=[4,6,9,11]
 cnt=0
-
+daynum = {'Mon': 0, 'Tue': 1, 'Wed':2, 'Thu':3,'Fri':4,'Sat':5,'Sun':6}
 for i in range(ar[0],ar[2]+1):
     if(i==2):
         cnt=cnt+29
@@ -20,5 +20,5 @@ elif(ar[2] in thirty):
     cnt=cnt-(30-ar[3])
 else:
     cnt=cnt-(31-ar[3])
-
+cnt = cnt-daynum[day]
 print(cnt//7+1)
