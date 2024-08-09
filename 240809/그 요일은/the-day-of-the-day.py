aa4329038155f4afa10faeaa0c502b9148e5f8e8
BackG <jw6133@naver.com>
2024-08-09 17:44:40 +0900

    Title: 그 요일은, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,23 @@
+ar = list(map(int, input().split()))
+day = str(input())
+
+thirty=[4,6,9,11]
+cnt=0
+
+for i in range(ar[0],ar[2]+1):
+    if(i==2):
+        cnt=cnt+29
+    elif(i in thirty):
+        cnt=cnt+30
+    else:
+        cnt=cnt+31
+
+
+if(ar[2]==2):
+    cnt=cnt-(29-ar[3])
+elif(ar[2] in thirty):
+    cnt=cnt-(30-ar[3])
+else:
+    cnt=cnt-(31-ar[3])
+
+print(cnt//7)
